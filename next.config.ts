c79a858ac4eb84ebf22aaa30fb4e6ce9ed145625
Your Name <you@example.com>
2026-03-20 18:28:36 +0800

    fix: github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 告诉 Next.js 我们要导出静态网页
+  output: 'export', // 关键：告诉 Next.js 导出静态页面
   images: {
-    unoptimized: true, // 这一行也是必须的，否则图片会报错
+    unoptimized: true, // 关键：关闭图片优化，否则 GitHub Pages 会报错
   },
+  // 注意：如果你的仓库名是 learning-os，这里需要加上一行：
+  basePath: '/learning-os',
 };
 
 export default nextConfig;
