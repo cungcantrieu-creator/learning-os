2645b99fcade062504d066bf6ec15076957ab511
Your Name <you@example.com>
2026-03-20 18:02:49 +0800

    fix: pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 告诉 Next.js 我们要导出静态网页
+  images: {
+    unoptimized: true, // 这一行也是必须的，否则图片会报错
+  },
 };
 
 export default nextConfig;
